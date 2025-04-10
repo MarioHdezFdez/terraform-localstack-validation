45c35b837d7285f31293b4424a9228a45fc4ca5a
mario <Mariof18@gmail.com>
2025-04-10 12:20:25 +0200

    Añadiendo configuración de Terraform y workflow para validación

---
+++ README.md
@@ -0,0 +1,5 @@
+# Avanzado Terraform
+
+Este repositorio contiene una configuración de Terraform con validación automática.
+
+![Terraform Validation](https://github.com/MarioHdezFdez/avanzado_terraform/actions/workflows/terraform-validate.yml/badge.svg)
---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+
+  endpoints {
+    s3 = "http://192.168.0.19:4566"
+  }
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "validation-bucket"
+}
